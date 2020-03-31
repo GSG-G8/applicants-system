01f3693d2df254e3225cbc346e633e731570e4fe
MohammedYehia <m-ya@windowslive.com>
2020-03-31 19:24:02 +0300

    Revert "create schema for validate tasks and regestrastion step in utel - valedate folder #"
    This reverts commit 47d7bb79c3a4d2f20a57960b6886695ad533ac32.

--- server/utils/validation/stepValidation.js
+++
@@ -1,8 +0,0 @@
-const yup = require('yup');
-
-const regStepSchema = yup.object().shape({
-  title: yup.string().required(),
-  details: yup.string().required(),
-});
-
-module.exports = regStepSchema;
