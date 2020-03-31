b4acc40bbe692c6001d570cd4fce16dc29000048
Ahmad Salah <44713857+ahmadysalah@users.noreply.github.com>
2020-03-31 23:45:33 +0300

    create schema for registration step and Technical task… (#65)
    * create schema again in spacifec file for regesration step and technecal task #56
    
    * create schema for validate tasks and regestrastion step in utel - valedate folder #
    #56
    
    * Revert "create schema for validate tasks and regestrastion step in utel - valedate folder #"
    
    This reverts commit 47d7bb79c3a4d2f20a57960b6886695ad533ac32.
    
    * remove valedate from utils #56
    
    * delete this file
    
    no need for this file for now , also the name of the file isn't correct.
    
    * delete file tchnecaltask and change the exportname of schema and all validate  #56
    
    * add trim to both file schema  #56
    
    Co-authored-by: MohammedYehia <m-ya@windowslive.com>
    Co-authored-by: Asmaa Thabet <asmathabet2009@hotmail.com>

---
+++ server/database/models/technicalTask.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const technicalTask = new Schema({
+  taskName: { type: String, required: true , trim: true,},
+  taskDescription: { type: String, required: true , trim: true,},
+});
+
+module.exports = mongoose.model('technicalTask', technicalTask);
