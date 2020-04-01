f39b3ea9da334b854a39de4d88137966344ef106
Ali <ali.dahdouh@gmail.com>
2020-04-01 15:57:26 +0300

    added error handling to get cohorts controllers relates #44

--- server/controllers/cohort/getCohorts.js
+++ server/controllers/cohort/getCohorts.js
@@ -2,12 +2,16 @@ const cohorts = require('../../database/models/cohort');
 
 module.exports = (req, res, next) => {
   cohorts
-    .find({})
-    .then((rows) =>
-      res.status(200).json({
-        statusCode: 200,
-        data: rows,
-      })
-    )
+    .find({}, (err, rows) => {
+      if (err) {
+        return res.status(400).json({ statusCode: 400, error: 'Bad Request' });
+      }
+      if (!rows.length) {
+        return res
+          .status(404)
+          .json({ statusCode: 404, error: `Cohorts not found` });
+      }
+      return res.status(200).json({ statusCode: 200, data: rows });
+    })
     .catch(next);
 };
