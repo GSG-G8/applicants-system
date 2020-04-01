2c56f2dd8eec826d54a5b12e8c957ceba74bb140
Ahmad Salah <a.ahmad.salah@gmail.com>
2020-04-01 16:12:55 +0300

    change layout for schema as all ather schemas in models for registrationStep and technicalTask

--- server/database/models/registrationStep.js
+++ server/database/models/registrationStep.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const registrationStep = new Schema({
-  title: { type: String, required: true, trim: true },
-  details: { type: String, required: true, trim: true },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  details: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 });
 
 module.exports = mongoose.model('registrationStep', registrationStep);
