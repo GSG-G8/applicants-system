d895a4975ffc5dd0068857ca12018209c81f45dc
Ahmad Salah <a.ahmad.salah@gmail.com>
2020-04-01 20:37:41 +0300

    change handle eror to send json #40

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -11,13 +11,14 @@ const login = (req, res) => {
   const { email, password } = req.body;
   loginValidation(email, password)
     .then((valid) => {
-      if (!valid) throw Error;
+      if (!valid)
+        res.status(400).json({ error: 'Please fill in all required fields' });
       else {
         applicant
           .findOne({ email })
           .then((data) => {
             bcrypt.compare(password, data.password, (err, result) => {
-              if (!result) res.status(400).json('Wrong Password');
+              if (!result) res.status(400).json({ error: 'Wrong Password' });
               else {
                 const userToken = { userId: data.id };
                 const cookie = sign(userToken, SECRET);
@@ -25,10 +26,14 @@ const login = (req, res) => {
               }
             });
           })
-          .catch(() => res.status(400).json('Check Username and Password'));
+          .catch(() =>
+            res.status(400).json({ error: 'Check Username and Password' })
+          );
       }
     })
-    .catch(() => res.status(400).json('Please Fill in all Required Fields'));
+    .catch(() =>
+      res.status(400).json({ error: 'Please fill in all required fields' })
+    );
 };
 
 module.exports = login;
