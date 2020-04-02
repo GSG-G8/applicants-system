a7a7a1b774ea19aeb0ebeff71e95e118e8360dbd
Ahmad Salah <a.ahmad.salah@gmail.com>
2020-04-02 10:56:43 +0300

    change the message in json to be more cleare and change the applicant router in index file #39

--- server/controllers/auth/logout.js
+++ server/controllers/auth/logout.js
@@ -1,5 +1,8 @@
 const logout = (req, res) => {
-  res.clearCookie('applicant').status(200).json('Logout successfully');
+  res
+    .clearCookie('applicant')
+    .status(200)
+    .json({ status: 'done', message: 'Logout Successfully' });
 };
 
 module.exports = logout;
--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const applicantRouter = require('./applicant-router');
 
-router.use('/api/v1/applicant', applicantRouter);
+router.use('applicant', applicantRouter);
 
 module.exports = router;
