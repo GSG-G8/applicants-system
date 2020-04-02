03cffdf5fdd6521bcc7be50f1a7830ea9e6bf9d4
Ali <ali.dahdouh@gmail.com>
2020-04-02 20:58:23 +0300

    removed unnecessary code block and refactored some lines relates #47

--- server/controllers/applicant/getApplicantsStats.js
+++ server/controllers/applicant/getApplicantsStats.js
@@ -1,15 +1,6 @@
 const Applicant = require('../../database/models/applicant');
 
 const getApplicantsStats = (req, res, next) => {
-  Applicant.find({}, (err, applicants) => {
-    if (err) {
-      return res.status(400).json({ statusCode: 400, error: 'Bad Request' });
-    }
-    if (!applicants.length) {
-      return res.status(404).json({ statusCode: 404, error: `Data not found` });
-    }
-    return true;
-  });
   Promise.all([
     Applicant.find({ accepted: true }).countDocuments(),
     Applicant.find({
@@ -19,17 +10,16 @@ const getApplicantsStats = (req, res, next) => {
       applicationSubmittedDate: null,
     }).countDocuments(),
   ])
-    .then((results) => {
-      const [accepted, submitted, notSubmitted] = results;
-      return res.status(200).json({
+    .then(([accepted, submitted, opened]) =>
+      res.status(200).json({
         statusCode: 200,
         Data: {
-          'Number of accepted': accepted,
-          'Number of submitted': submitted,
-          'Number of opened': notSubmitted,
+          accepted,
+          submitted,
+          opened,
         },
-      });
-    })
+      })
+    )
     .catch(next);
 };
 
