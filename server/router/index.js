a980e6a1066b2cd793ac5b9dff609611585eb42b
mflaifel <msf-2oo7@hotmail.com>
2020-04-03 01:13:21 +0300

    solve dummy problem in router index relates #23

--- server/router/index.js
+++ server/router/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const applicantRouter = require('./applicant-router');
+const getCohorts = require('./cohort-router');
 
 router.use(applicantRouter);
 
-const getCohorts = require('./cohort-router');
-const applicantRouter = require('./applicant-router');
-
 router.get('/cohorts', getCohorts);
 
-router.use('/',applicantRouter);
+router.use('/', applicantRouter);
 
 module.exports = router;
