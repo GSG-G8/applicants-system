74176fbf7a81a19b9082f905143be678318e3e22
mflaifel <msf-2oo7@hotmail.com>
2020-04-03 00:57:57 +0300

    update sign validation to include location and full name relates #23

--- server/utils/validation/applicantSignupValidation.js
+++ server/utils/validation/applicantSignupValidation.js
@@ -1,21 +1,33 @@
 const yup = require('yup');
 
 const schema = yup.object().shape({
+  fullName: yup.string(),
   email: yup.string().email().min(4).required(),
   password: yup
     .string()
-    .matches(/[a-zA-Z0-9]/)
-    .min(6)
-    .required(),
+    .required('Please Enter your password')
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
+    ),
   passwordConfirmation: yup
     .mixed()
     .oneOf([yup.ref('password')])
     .required(),
+  location: yup.mixed().oneOf(['gaza', 'khalil']).required(),
 });
 
-exports.signupValidate = (email, password, passwordConfirmation) =>
+exports.signupValidate = (
+  fullName,
+  email,
+  password,
+  passwordConfirmation,
+  location
+) =>
   schema.isValid({
+    fullName,
     email,
     password,
     passwordConfirmation,
+    location,
   });
