168d6f1505b1760b451c2df66b56549ba7c53103
mflaifel <msf-2oo7@hotmail.com>
2020-04-02 13:06:46 +0300

    remove line 7 from index.js in router #23

--- server/router/index.js
+++ server/router/index.js
@@ -4,7 +4,7 @@ const getCohorts = require('./cohort-router');
 const signupApplicant = require('./applicant-router');
 
 router.get('/cohorts', getCohorts);
-gi
+
 router.post('/signup', signupApplicant);
 
 module.exports = router;
