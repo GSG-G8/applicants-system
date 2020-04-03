f6a314405c769d870e6c1f5cc8c191e7e9017184
Asmaathabet <asmathabet2009@hotmail.com>
2020-04-03 22:26:56 +0300

    add empty line between authRouter and others

--- server/router/applicant-router.js
+++ server/router/applicant-router.js
@@ -1,7 +1,3 @@
 const applicantRouter = require('express').Router();
 
-const { getApplicantsStats } = require('../controllers/applicant');
-
-applicantRouter.get('/applicants/stats', getApplicantsStats);
-
 module.exports = applicantRouter;
