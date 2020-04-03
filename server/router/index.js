0fb8f4de15c8a53ae0f73d4758484d721b95b588
Ali <ali.dahdouh@gmail.com>
2020-04-03 20:50:07 +0300

    fixed moudule requires in router relates #47

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 
+const authRouter = require('./auth-router');
 const applicantRouter = require('./applicant-router');
 const cohortRouter = require('./cohort-router');
-const authRouter = require('./auth-router');
 
 router.use(authRouter);
-router.use(cohortRouter);
-
 router.use(applicantRouter);
+router.use(cohortRouter);
 
 module.exports = router;
