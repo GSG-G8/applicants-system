a4e0ce1948bc37bd17df54b1f34775f728fdeb69
mFlaifel <msf-2oo7@hotmail.com>
2020-04-06 23:39:53 +0300

    edit response when the user is not authorized relates #84

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -7,7 +7,9 @@ require('env2')('config.env');
 const isAuthorized = (req, res, next) => {
   jwt.verify(req.cookies.applicant, process.env.SECRET_KEY, (err, token) => {
     if (err) {
-      res.status(401).send({ statusCode: 401, error: 'you are Unauthorized' });
+      res
+        .status(401)
+        .send({ statusCode: 401, auth: false, error: 'you are Unauthorized' });
     } else {
       res.status(200).send({ auth: true, id: token.id });
       next();
@@ -19,13 +21,17 @@ const isAdmin = (req, res, next) => {
   const { email } = req.body;
   admin.findOne({ email }).then((data) => {
     if (!data.email) {
-      res.status(401).send({ statusCode: 401, error: 'you are Unauthorized' });
+      res
+        .status(401)
+        .send({ statusCode: 401, auth: false, error: 'you are Unauthorized' });
     } else {
       jwt.verify(req.cookies.admin, process.env.SECRET_KEY, (err, token) => {
         if (err) {
-          res
-            .status(401)
-            .send({ statusCode: 401, error: 'you are Unauthorized' });
+          res.status(401).send({
+            statusCode: 401,
+            auth: false,
+            error: 'you are Unauthorized',
+          });
         } else {
           res.status(200).send({ auth: true, id: token.id });
           next();
