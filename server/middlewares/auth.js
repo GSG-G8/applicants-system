b6dcad75ff375e1051f8dbea1db1315a9df4ae0e
mFlaifel <msf-2oo7@hotmail.com>
2020-04-06 21:32:15 +0300

    edite typing error in auth.js relates #84

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -7,7 +7,7 @@ require('env2')('config.env');
 const isAuthorized = (req, res, next) => {
   jwt.verify(req.cookies.applicant, process.env.SECRET_KEY, (err, token) => {
     if (err) {
-      res.status(401).send({ statusCode: 401, error: 'you are un authorized' });
+      res.status(401).send({ statusCode: 401, error: 'you are Unauthorized' });
     } else {
       res.status(200).send({ auth: true, id: token.id });
       next();
@@ -19,7 +19,7 @@ const isAdmin = (req, res, next) => {
   const { email } = req.body;
   admin.findOne({ email }).then((data) => {
     if (!data.email) {
-      res.status(401).send({ statusCode: 401, error: 'you are un authorized' });
+      res.status(401).send({ statusCode: 401, error: 'you are Unauthorized' });
     } else {
       jwt.verify(req.cookies.admin, process.env.SECRET_KEY, (err, token) => {
         if (err) {
