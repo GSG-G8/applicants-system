6685a980f5fba11eb532a99e237420e394f3c712
Ali <ali.dahdouh@gmail.com>
2020-04-07 23:03:03 +0300

    fixed naming relates #99

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,6 @@ const applicant = require('./applicant');
 const cohorts = require('./cohort/getCohorts');
 const admin = require('./admin');
 const errors = require('./errors');
-const getRegistrationSteps = require('./registrationSteps/getRegistrationSteps');
+const registrationSteps = require('./registrationSteps/getRegistrationSteps');
 
-module.exports = { cohorts, applicant, admin, getRegistrationSteps, errors };
+module.exports = { cohorts, applicant, admin, registrationSteps, errors };
