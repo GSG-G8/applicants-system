269c9df6774156d71baf0f403e00b1d70eeba069
mFlaifel <msf-2oo7@hotmail.com>
2020-04-07 12:41:25 +0300

    update router accordin to pull request comment #95

--- server/router/index.js
+++ server/router/index.js
@@ -2,19 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-const { login, logout, signup } = require('./auth-router');
+const { login, logout } = require('./auth-router');
 const adminRouter = require('./admin-router');
 const applicantRouter = require('./applicant-router');
 const cohortRouter = require('./cohort-router');
 const errorHandler = require('../controllers/errors');
 const { isAuthorized, isAdmin } = require('../middlewares/auth');
 
-router.post('/signup, signup');
 router.post('/login', login);
 
 router.use('/dashboard', isAdmin, adminRouter);
 
-router.use('/cohorts', isAdmin, cohortRouter);
+router.use(cohortRouter);
 
 router.use('/applicants', isAuthorized, applicantRouter);
 
