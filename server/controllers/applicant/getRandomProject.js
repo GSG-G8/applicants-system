429d690ddf48aee06cd6da6ab7591693fa15dd0a
Asmaathabet <asmathabet2009@hotmail.com>
2020-04-08 02:48:18 +0300

    add some changes & remove mongoose random package relates #103

--- server/controllers/applicant/getRandomProject.js
+++ server/controllers/applicant/getRandomProject.js
@@ -1,13 +1,22 @@
 const project = require('../../database/models/project');
 
-const randomProject = (req, res) => {
-  project.aggregate([{ $sample: { size: 1 } }], (err, result) => {
-    if (err)
-      res
-        .status(400)
-        .json({ status: 'Failed', message: 'There is no projects' });
-    else res.status(200).json({ status: 'successfully', project: result });
-  });
+const randomProject = (req, res, next) => {
+  project
+    .aggregate([{ $sample: { size: 1 } }], (err, result) => {
+      if (err)
+        res
+          .status(400)
+          .json({ status: 'Failed', message: 'There is no projects' });
+
+      if (!result.length) {
+        return res
+          .status(404)
+          .json({ statusCode: 404, error: `Project not found` });
+      }
+
+      return res.status(200).json({ status: 'successfully', project: result });
+    })
+    .catch(next);
 };
 
 module.exports = randomProject;
