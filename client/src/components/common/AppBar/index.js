c3a51d13a2c888adf5038347a99ef86cd95fb00b
Ali <ali.dahdouh@gmail.com>
2020-04-09 19:41:14 +0300

    fixed naming relates #112

--- client/src/components/common/AppBar/index.js
+++ client/src/components/common/AppBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
+import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import logo from '../../../assets/images/logo.png';
@@ -7,12 +7,12 @@ import './index.css';
 
 const NavBar = () => (
   <div className="root">
-    <AppBar color="transparent">
+    <MuiAppBar color="transparent">
       <Toolbar className="Toolbar">
         <img src={logo} alt="logo" className="Logo" />
         <AccountCircle />
       </Toolbar>
-    </AppBar>
+    </MuiAppBar>
   </div>
 );
 
