602521092b45fbe306d649cc3c1a887f19ef78c5
Ali <ali.dahdouh@gmail.com>
2020-04-09 17:50:58 +0300

    fixed styling relates #112

--- client/src/components/common/AppBar/index.js
+++ client/src/components/common/AppBar/index.js
@@ -1,40 +1,19 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import logo from '../../../assets/images/logo.png';
+import './index.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    position: 'fixed',
-  },
-  Toolbar: {
-    justifyContent: 'space-between',
-    minHeight: 111,
-    marginRight: theme.spacing(2),
-    marginLeft: theme.spacing(2),
-  },
-  Logo: {
-    pointerEvents: 'none',
-    width: '25%',
-  },
-}));
-
-const NavBar = () => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar color="transparent">
-        <Toolbar className={classes.Toolbar}>
-          <img src={logo} alt="logo" className={classes.Logo} />
-          <AccountCircle />
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
+const NavBar = () => (
+  <div className="root">
+    <AppBar color="transparent">
+      <Toolbar className="Toolbar">
+        <img src={logo} alt="logo" className="Logo" />
+        <AccountCircle />
+      </Toolbar>
+    </AppBar>
+  </div>
+);
 
 export default NavBar;
