5d6415ea8cfaef878a058908579ab3a8cf5714f5
Ahmad Salah <a.ahmad.salah@gmail.com>
2020-04-11 21:11:42 +0300

    delete disabled file and put it as props and add props like onchang and placeholder and className #117

--- client/src/components/common/TextField/index.js
+++ client/src/components/common/TextField/index.js
@@ -1,35 +1,59 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
-import InputDisabled from './textdisabled';
 import style from './style';
 
-function InputText({ id, type, label, value }) {
+function InputText({
+  id,
+  type,
+  label,
+  value,
+  className,
+  message,
+  placeholder,
+  onChange,
+  disabled,
+}) {
   const classes = style();
+
   return (
     <div className={classes.root}>
       <TextField
+        disabled={disabled}
         id={id}
         label={label}
+        placeholder={placeholder}
         defaultValue={value}
         type={type}
-        className={classes.container}
+        helperText={message}
+        className={`classes.container ${className}`}
         variant="outlined"
+        onChange={onChange}
       />
     </div>
   );
 }
-InputText.defaultProps = {
-  id: 'outlined-required',
-  type: '',
-  label: '',
-  value: '',
-};
 
 InputText.propTypes = {
   id: PropTypes.string,
   type: PropTypes.string,
   value: PropTypes.string,
   label: PropTypes.string,
+  className: PropTypes.string,
+  message: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
 };
-export { InputText, InputDisabled };
+InputText.defaultProps = {
+  id: 'outlined-required',
+  type: '',
+  label: '',
+  value: '',
+  className: '',
+  message: '',
+  placeholder: '',
+  onChange: '',
+  disabled: false,
+};
+export default InputText;
