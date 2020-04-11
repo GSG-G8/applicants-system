9a99c931b34bc84ed980aaaa6bc4dd6f61883274
Hassan Al-najjar <ha77an19@gmail.com>
2020-04-11 23:28:25 +0300

    added props validation and default values

--- client/src/components/common/card/index.js
+++ client/src/components/common/card/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import clsx from 'clsx';
+import CardContent from '@material-ui/core/CardContent';
+import PropTypes from 'prop-types';
 import useStyles from './style';
 
-const SimpleCard = ({ classname = 'card_steper', content }) => {
+const SimpleCard = ({ ClassName, content }) => {
   const classes = useStyles();
-  return <Card className={classes[`${classname}`]}>{content}</Card>;
-}
+  return (
+    <Card className={clsx(classes.card_steper, ClassName)}>
+      <CardContent>{content}</CardContent>
+    </Card>
+  );
+};
 
+SimpleCard.propTypes = {
+  ClassName: PropTypes.string,
+  content: PropTypes.string,
+};
+SimpleCard.defaultProps = {
+  ClassName: ' ',
+  content: ' ',
+};
 export default SimpleCard;
