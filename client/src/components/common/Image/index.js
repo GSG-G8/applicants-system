25842490074a04840597840d989ad92c11ffea77
mFlaifel <msf-2oo7@hotmail.com>
2020-04-12 22:22:51 +0300

    update image component according to PR comments relates #115

--- client/src/components/common/Image/index.js
+++ client/src/components/common/Image/index.js
@@ -1,23 +1,15 @@
 import Image from 'material-ui-image';
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-const CommonImage = ({
-  src,
-  width,
-  height,
-  animationDuration,
-  onClick,
-  alt,
-  style,
-}) => (
+const CommonImage = ({ src, width, height, onClick, alt, style }) => (
   <Image
+    src={src}
     imageStyle={{ width, height }}
-    animationDuration={animationDuration}
     onClick={onClick}
     alt={alt}
     style={style}
-    src={src}
+    animationDuration="0"
   />
 );
 
@@ -25,16 +17,14 @@ CommonImage.propTypes = {
   src: PropTypes.string.isRequired,
   width: PropTypes.string,
   height: PropTypes.string,
-  animationDuration: PropTypes.number,
   onClick: PropTypes.func,
   alt: PropTypes.string,
   style: PropTypes.string,
 };
 CommonImage.defaultProps = {
-  animationDuration: 0,
   width: '100%',
   height: 'auto',
-  onClick: null,
+  onClick: () => {},
   alt: 'application custom image',
   style: null,
 };
