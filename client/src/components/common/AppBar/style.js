3e71b2b5f5685362a9d3a82ae0b64813c35f4b3a
Ali <ali.dahdouh@gmail.com>
2020-04-12 17:23:05 +0300

    removed profile icon relates #112

--- client/src/components/common/AppBar/style.js
+++ client/src/components/common/AppBar/style.js
@@ -2,11 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'fixed',
+    position: 'absolute',
+    zIndex: '1',
   },
   toolbar: {
     justifyContent: 'space-between',
-    margin: theme.spacing(2),
   },
   logo: {
     pointerEvents: 'none',
