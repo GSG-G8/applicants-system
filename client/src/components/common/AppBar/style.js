4bb61e565f98c4a942a33aa8b9c2f2f2e53f4d63
Ali <ali.dahdouh@gmail.com>
2020-04-12 23:47:26 +0300

    removed unused styles relates #112

--- client/src/components/common/AppBar/style.js
+++ client/src/components/common/AppBar/style.js
@@ -2,8 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'absolute',
-    zIndex: '1',
+    position: 'fixed',
   },
   toolbar: {
     justifyContent: 'space-between',
