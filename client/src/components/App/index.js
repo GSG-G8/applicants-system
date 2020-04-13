b857b52d50158a7eb368f6f3d747e621b8bf7402
Hassan Al-najjar <ha77an19@gmail.com>
2020-04-13 15:28:48 +0300

    FrontEnd Handling Routes relates #137

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import axios from 'axios';
+
 import './index.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Welcome in Applicants System</h1>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  state = {
+    auth: null,
+  };
 
-export default App;
+  componentDidMount() {
+    axios
+      .get('/api/v1/auth')
+      .then((result) => {
+        this.setState({ ...result.data });
+      })
+      .catch(() => {
+        this.setState({ auth: false });
+      });
+  }
+
+  signupHandler = () => {
+    this.setState({
+      auth: true,
+    });
+  };
+
+  loginHandler = () => {
+    axios.get('/api/v1/login').then(() => {
+      this.setState({
+        auth: false,
+      });
+    });
+  };
+
+  logoutHandler = () => {
+    axios.get('/api/v1/logout').then(() => {
+      this.setState({
+        auth: false,
+      });
+    });
+  };
+
+  render() {
+    const { auth } = this.state;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/404">404</Route>
+          <Route path="/500">500</Route>
+          {auth === false ? (
+            <Switch>
+              <Route exact path="/" render={(props) => <div> Home </div>} />
+              <Route
+                exact
+                path="/login"
+                render={(props) => <div> login </div>}
+              />
+
+              <Route exact path="/signup" render={() => <div> signup </div>} />
+              <Route render={() => <Redirect to="/" />} />
+            </Switch>
+          ) : (
+            <Switch>
+              <Route path="/steps">steps</Route>
+              <Route path="/availability">availability</Route>
+              <Route path="/tasks">tasks</Route>
+              <Route path="/project">project</Route>
+              <Route path="/submit">submit</Route>
+              <Route path="/myprofile">myprofile</Route>
+              <Route path="/dashboard">dashboard</Route>
+              <Route path="/dashboard/applications/opened">opened</Route>
+              <Route path="/dashboard/applications/submitted">submitted</Route>
+              <Route path="/dashboard/applications/submitted/:applicantID">
+                applicantID
+              </Route>
+              <Route path="/dashboard/applications/completed">completed</Route>
+            </Switch>
+          )}
+        </Switch>
+      </Router>
+    );
+  }
+}
