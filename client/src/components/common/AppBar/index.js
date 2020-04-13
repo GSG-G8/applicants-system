9e9be6e7e13801a67dd7c2f6068df2221aa9f1d0
Ali <ali.dahdouh@gmail.com>
2020-04-13 09:34:37 +0300

    fixed usermenu to check the state relates #112

--- client/src/components/common/AppBar/index.js
+++ client/src/components/common/AppBar/index.js
@@ -2,21 +2,27 @@ import React from 'react';
 import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import logo from '../../../assets/images/logo1.svg';
-import useStyles from './style';
 import UserMenu from '../UserMenu';
+import './style.css';
 
-const AppBar = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <MuiAppBar color="transparent">
-        <Toolbar className={classes.toolbar}>
-          <img src={logo} alt="Logo" className={classes.logo} />
-          <UserMenu isAuth />
-        </Toolbar>
-      </MuiAppBar>
-    </div>
-  );
-};
+class AppBar extends React.Component {
+  state = {
+    isloggedin: true,
+  };
+
+  render() {
+    const { isloggedin } = this.state;
+    return (
+      <div className="root">
+        <MuiAppBar color="transparent">
+          <Toolbar className="toolbar">
+            <img src={logo} alt="Logo" className="logo" />
+            {isloggedin && <UserMenu />}
+          </Toolbar>
+        </MuiAppBar>
+      </div>
+    );
+  }
+}
 
 export default AppBar;
--- client/src/components/common/UserMenu/index.js
+++ client/src/components/common/UserMenu/index.js
@@ -3,9 +3,8 @@ import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
-import PropTypes from 'prop-types';
 
-const UserMenu = ({ isAuth }) => {
+const UserMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -18,39 +17,27 @@ const UserMenu = ({ isAuth }) => {
   };
   return (
     <div>
-      {isAuth && (
-        <div>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleMenu}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            keepMounted
-            open={open}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>See Profile</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
-        </div>
-      )}
+      <IconButton
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>See Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
     </div>
   );
-};
-
-UserMenu.propTypes = {
-  isAuth: PropTypes.bool,
-};
-
-UserMenu.defaultProps = {
-  isAuth: '',
 };
 
 export default UserMenu;
