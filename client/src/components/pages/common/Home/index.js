9be1523a4ffddad33e929b2f5c7acd965945225e
Ali <ali.dahdouh@gmail.com>
2020-04-16 00:32:30 +0300

    fixed calling data relates #25

--- client/src/components/pages/common/Home/index.js
+++ client/src/components/pages/common/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DotsStepper from '../../../application/DotsStepper';
-import registrationSteps from '../../../application/DotsStepper/registrationSteps.json';
+import { registrationSteps } from '../../../application/DotsStepper/registrationSteps.json';
 import draw1 from '../../../../assets/images/draw1.svg';
 
 import './index.css';
@@ -8,7 +8,7 @@ import './index.css';
 const Home = () => (
   <>
     <div className="root">
-      <DotsStepper steps={registrationSteps.registrationSteps} />
+      <DotsStepper steps={registrationSteps} />
 
       <img className="img" src={draw1} alt="background" />
     </div>
