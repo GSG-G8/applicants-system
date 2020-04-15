50b5dcccf0d695df247bb242470ea2e89ccdff28
mFlaifel <msf-2oo7@hotmail.com>
2020-04-16 02:12:42 +0300

    fix eslint error in loginValidtion file relates #30

--- client/src/utils/application/LoginValidation.js
+++ client/src/utils/application/LoginValidation.js
@@ -1,13 +1,13 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  email: yup.string().email().required("Please Enter your email"),
+  email: yup.string().email().required('Please Enter your email'),
   password: yup
     .string()
-    .required("Please Enter your password")
+    .required('Please Enter your password')
     .matches(
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
     ),
 });
 const loginValidate = ({ email, password }) =>
