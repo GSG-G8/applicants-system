f241832c18df8f35649174fce74d7ee31b8ec2e9
Ali <ali.dahdouh@gmail.com>
2020-04-15 23:42:44 +0300

    added svg, stepper and made some styling relates #25

--- client/src/components/pages/common/Home/index.js
+++ client/src/components/pages/common/Home/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import DotsStepper from '../../../application/DotsStepper';
+import registrationSteps from '../../../application/DotsStepper/registrationSteps.json';
+import draw1 from '../../../../assets/images/draw1.svg';
 
-const Home = () => <h1>Home</h1>;
+import './index.css';
 
+const Home = () => (
+  <>
+    <div className="root">
+      <DotsStepper steps={registrationSteps.registrationSteps} />
+
+      <img className="img" src={draw1} alt="background" />
+    </div>
+  </>
+);
 export default Home;
