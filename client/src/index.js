bea6a9ef7af4e1625735fb03870078e1262c286e
Ali <ali.dahdouh@gmail.com>
2020-06-02 12:29:35 +0300

    fixed App importing relates #116

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App/index';
+import App from './components/App';
 
 ReactDOM.render(
   <BrowserRouter>
