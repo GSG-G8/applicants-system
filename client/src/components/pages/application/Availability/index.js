1556ecb49e5a933733126185e0f39297066b55ec
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-02 21:57:07 +0300

    create availability page and add style beside add the nonlineareStepper inside the page #37

--- client/src/components/pages/application/Availability/index.js
+++ client/src/components/pages/application/Availability/index.js
@@ -1,5 +1,110 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import NonLinearStepper from '../../../application/NonlinearStepper';
+import Alert from '../../../common/Alert';
+import Typography from '../../../common/Typography';
+import Button from '../../../common/Button';
+import backGround from '../../../../assets/images/backGround.svg';
+import { availability } from './availability.json';
+import './index.css';
 
-const Availability = () => <h1>Availability</h1>;
+const Availability = ({ fullName, userID }) => {
+  const [available, setAvailable] = useState(false);
+  const [alert, throwAlert] = useState(false);
+  const [nonLinear, setNonLinear] = useState(false);
+  const history = useHistory();
+
+  const next = async () => {
+    if (!available) throwAlert(true);
+    else
+      try {
+        await axios.patch(`/api/v1/applicants/5e847f8a49abf469bd96009c`, {
+          available,
+        });
+        setNonLinear(true);
+      } catch (error) {
+        throwAlert(true);
+      }
+  };
+
+  useEffect(() => {});
+
+  return (
+    <div className="Container_page">
+      <Helmet>
+        <title>Availability</title>
+      </Helmet>
+      <img src={backGround} alt="backGround" className="backGround" />
+      <div className="Container_page__content">
+        {!nonLinear ? (
+          <>
+            <div className="text_Welcome">
+              <Typography variant="h4" color="default">
+                Welcome, {fullName}
+              </Typography>
+            </div>
+            <div className="availability__head">
+              <Typography variant="h6" color="default" align="left">
+                Availability
+              </Typography>
+            </div>
+            <div className="availability">
+              <Typography variant="body2" align="left">
+                {availability.split('.')[0]}.
+              </Typography>
+              <Typography variant="body2" align="left">
+                {availability.split('.')[1]}.
+              </Typography>{' '}
+              <Typography variant="body2" align="right">
+                {availability.split('.')[2]}.
+              </Typography>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={available}
+                    onChange={(event) => setAvailable(event.target.checked)}
+                    name="availability"
+                    color="secondary"
+                  />
+                }
+                label={
+                  <Typography variant="body2" align="left">
+                    {availability.split('.')[3]}.
+                  </Typography>
+                }
+              />
+            </div>
+            <div className="availability__buttons">
+              <Button onClick={() => history.push('/steps')}>Back</Button>
+              <Button customStyle="outlined" onClick={() => next()}>
+                Next
+              </Button>
+            </div>
+            {alert && (
+              <Alert
+                Type="warning"
+                Msg="You should check Availability before going next"
+              />
+            )}
+          </>
+        ) : (
+          <div className="availability__stepper">
+            <NonLinearStepper userID={userID} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Availability.propTypes = {
+  fullName: PropTypes.string.isRequired,
+  userID: PropTypes.string.isRequired,
+};
 
 export default Availability;
