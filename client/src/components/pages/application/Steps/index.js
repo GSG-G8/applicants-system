e163236c2e5ff004184db57a261e2e7de44891bb
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-02 14:54:06 +0300

    change page layout with limitation and change the app route and add the left drawer beside fix some bugs in use effect in list file #34

--- client/src/components/pages/application/Steps/index.js
+++ client/src/components/pages/application/Steps/index.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import Limitation from '../../../common/limitation';
 import Typography from '../../../common/Typography';
 import Button from '../../../common/Button';
 import backGround from '../../../../assets/images/backGround.svg';
-
 import './style.css';
 
 const getSteps = async () => {
-  const { data } = (await axios.get('/api/v1//steps')).data;
+  const { data } = (await axios.get('/api/v1/steps')).data;
   return data;
 };
 
@@ -26,6 +26,9 @@ const Steps = ({ fullName }) => {
 
   return (
     <div className="Container_page">
+      <Helmet>
+        <title>SignUp</title>
+      </Helmet>
       <img src={backGround} alt="backGround" className="backGround" />
       <div className="Container_page__content">
         <div className="text_Welcome">
@@ -34,9 +37,11 @@ const Steps = ({ fullName }) => {
           </Typography>
         </div>
         <div className="steps">
-          <Typography variant="h6" color="default" align="left">
-            Application STEPS
-          </Typography>
+          {steps && (
+            <Typography variant="h6" color="default" align="left">
+              Application STEPS
+            </Typography>
+          )}
           {steps ? (
             steps.map(({ title, details }) => (
               <ul className="steps__list">
@@ -51,9 +56,11 @@ const Steps = ({ fullName }) => {
             <Limitation />
           )}
         </div>
-        <div className="steps_buttons">
-          <Button onClick={() => history.push('/availability')}>Next</Button>
-        </div>
+        {steps && (
+          <div className="steps_buttons">
+            <Button onClick={() => history.push('/availability')}>Next</Button>
+          </div>
+        )}
       </div>
     </div>
   );
