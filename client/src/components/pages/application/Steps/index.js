7c809ffff1ee203a098078755810da2b2e85375a
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-02 13:57:38 +0300

    create the steps route and make style beside use xios to get the data from database #34

--- client/src/components/pages/application/Steps/index.js
+++ client/src/components/pages/application/Steps/index.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Limitation from '../../../common/limitation';
 import Typography from '../../../common/Typography';
 import Button from '../../../common/Button';
 import backGround from '../../../../assets/images/backGround.svg';
+
 import './style.css';
 
 const getSteps = async () => {
@@ -25,31 +27,40 @@ const Steps = ({ fullName }) => {
   return (
     <div className="Container_page">
       <img src={backGround} alt="backGround" className="backGround" />
-      <div className="text_Welcome">
-        <Typography variant="h3" color="default">
-          Welcome, {fullName}
-        </Typography>
-      </div>
-      {steps &&
-        steps.map(({ title, details }) => (
-          <ul className="steps__list">
-            <li>
-              {title} <strong> {details}</strong>
-            </li>
-          </ul>
-        ))}
-      <div className="container_buttons">
-        <Button onClick={() => history.push('/availability')}>Next</Button>
+      <div className="Container_page__content">
+        <div className="text_Welcome">
+          <Typography variant="h4" color="default">
+            Welcome, {fullName}
+          </Typography>
+        </div>
+        <div className="steps">
+          <Typography variant="h6" color="default" align="left">
+            Application STEPS
+          </Typography>
+          {steps ? (
+            steps.map(({ title, details }) => (
+              <ul className="steps__list">
+                <li>
+                  <Typography variant="body2" align="left">
+                    {title} {details}
+                  </Typography>
+                </li>
+              </ul>
+            ))
+          ) : (
+            <Limitation />
+          )}
+        </div>
+        <div className="steps_buttons">
+          <Button onClick={() => history.push('/availability')}>Next</Button>
+        </div>
       </div>
     </div>
   );
 };
 
 Steps.propTypes = {
-  fullName: PropTypes.string,
-};
-Steps.defaultProps = {
-  fullName: 'Ahmad',
+  fullName: PropTypes.string.isRequired,
 };
 
 export default Steps;
