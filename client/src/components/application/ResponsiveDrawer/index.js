850728b6e56ecb6cff301cb564ade8f256a8bcb8
ha77an <ha77an19@gmail.com>
2020-06-05 20:14:29 +0300

    Made Drawer responsive with all sccreens

--- client/src/components/application/ResponsiveDrawer/index.js
+++ client/src/components/application/ResponsiveDrawer/index.js
@@ -1,32 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import PropTypes from 'prop-types';
+import { useTheme } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import DrawerList from './list';
 import useStyles from './style';
 
-const ResponsiveDrawer = ({ Content }) => {
+const ResponsiveDrawer = ({ Content, window }) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const theme = useTheme();
   const classes = useStyles();
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   return (
     <div>
-      <Hidden implementation="js">
-        <Drawer
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          variant="persistent"
-          open
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          className={classes.menuButton}
         >
-          <DrawerList content={Content} />
-        </Drawer>
-      </Hidden>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true,
+            }}
+          >
+            <DrawerList content={Content} />
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="js">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            <DrawerList content={Content} />
+          </Drawer>
+        </Hidden>
+      </nav>
     </div>
   );
 };
 
 ResponsiveDrawer.propTypes = {
   Content: PropTypes.node,
+  // eslint-disable-next-line react/require-default-props
+  window: PropTypes.func,
 };
 ResponsiveDrawer.defaultProps = {
   Content: [
