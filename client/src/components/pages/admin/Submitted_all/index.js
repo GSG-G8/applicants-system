1ee044c97a8205f6bda3a00a0a36034eaa8c7ab7
ahmadysalah <a.ahmad.salah@gmail.com>
2020-06-07 21:22:00 +0300

    create submitted application page with table commponent to show all submitted beside search and sort also padding #51

--- client/src/components/pages/admin/Submitted_all/index.js
+++ client/src/components/pages/admin/Submitted_all/index.js
@@ -1,5 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import { Helmet } from 'react-helmet';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import backGround from '../../../../assets/images/backgroundDash.svg';
+import DashBar from '../../../dashboard/Tabs';
 
-const SubmittedAll = () => <h1>SubmittedAll</h1>;
+import './index.css';
 
-export default SubmittedAll;
+const getSubmitted = async () => {
+  const { data } = (await axios.get('/api/v1/dashboard/applicants')).data;
+  return data;
+};
+
+const Header = {
+  columns: [
+    { title: 'Name', field: 'fullName', width: '20%' },
+    { title: 'Email', field: 'email', width: '20%' },
+    { title: 'Mobile', field: 'mobileNumber' },
+    { title: 'Age', field: 'age' },
+    { title: 'Address', field: 'address' },
+    { title: 'Status', render: () => 'Submitted' },
+    {
+      title: 'Profile',
+      render: ({ _id }) => (
+        <Link
+          className="profile_Link"
+          exact
+          to={`/dashboard/applications/submitted/${_id}`}
+        >
+          Profile
+        </Link>
+      ),
+    },
+  ],
+};
+
+const SubmittedApplications = () => {
+  const [submitted, setSubmitted] = useState();
+
+  useEffect(() => {
+    if (!submitted) getSubmitted().then(setSubmitted);
+  }, [submitted]);
+
+  return (
+    <>
+      <Helmet>
+        <title>Submitted Applications</title>
+      </Helmet>
+      <DashBar />
+      <img src={backGround} alt="backGround" className="dash__background" />
+      <div className="dashboard__page">
+        <div className="submitted__container">
+          <MaterialTable
+            className="submitted__table"
+            title="Submitted Applications"
+            columns={Header.columns}
+            data={submitted && submitted}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+export default SubmittedApplications;
