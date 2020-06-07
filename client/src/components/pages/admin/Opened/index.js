7853236e4a911bc3a2862cf3716f1346de5d3869
Ali <ali.dahdouh@gmail.com>
2020-06-08 00:36:37 +0300

    added opened applications page relates #52

--- client/src/components/pages/admin/Opened/index.js
+++ client/src/components/pages/admin/Opened/index.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import { Helmet } from 'react-helmet';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import backGround from '../../../../assets/images/backgroundDash.svg';
+import DashBar from '../../../dashboard/Tabs';
 
-const Opened = () => <h1>Opened </h1>;
+import './index.css';
 
+const getOpened = async () => {
+  const { data } = (await axios.get('/api/v1/dashboard/applicants')).data;
+  return data.filter((element) => !element.applicationSubmittedDate && element);
+};
+
+const Header = {
+  columns: [
+    { title: 'Name', field: 'fullName', width: '20%' },
+    { title: 'Email', field: 'email', width: '20%' },
+    { title: 'Mobile', field: 'mobileNumber' },
+    { title: 'Age', field: 'age' },
+    { title: 'Academy Location', field: 'location' },
+    { title: 'Address', field: 'address' },
+    {
+      title: 'Profile',
+      render: ({ _id }) => (
+        <Link
+          className="profile_Link"
+          exact
+          to={`/dashboard/applications/submitted/${_id}`}
+        >
+          View
+        </Link>
+      ),
+    },
+  ],
+};
+
+const Opened = () => {
+  const [opened, setOpened] = useState();
+
+  useEffect(() => {
+    if (!opened) getOpened().then(setOpened);
+  }, [opened]);
+
+  return (
+    <>
+      <Helmet>
+        <title>Submitted Applications</title>
+      </Helmet>
+      <DashBar />
+      <img src={backGround} alt="backGround" className="dash__background" />
+      <div className="dashboard__page">
+        <div className="submitted__container">
+          <MaterialTable
+            className="submitted__table"
+            title="Submitted Applications"
+            columns={Header.columns}
+            data={opened}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+getOpened.propTypes = {
+  _id: PropTypes.string.isRequired,
+};
 export default Opened;
