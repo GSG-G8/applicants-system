6f494dddb3ae08b816262df0d6333b208b49b6f5
mFlaifel <msf-2oo7@hotmail.com>
2020-06-08 07:43:20 +0300

    create completed application page - this page is clone from submitted Application with the needed modfications #49

--- client/src/components/pages/admin/Completed/index.js
+++ client/src/components/pages/admin/Completed/index.js
@@ -1,5 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import { Helmet } from 'react-helmet';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import backGround from '../../../../assets/images/backgroundDash.svg';
 
-const Completed = () => <h1>Completed</h1>;
+import './index.css';
+
+const getSubmitted = async () => {
+  const { data } = (
+    await axios.get(
+      '/api/v1/dashboard/applicants/query?submitted=true&fccpoints=220&cwscore=5&covered=true'
+    )
+  ).data;
+  return data.filter((element) => !!element.applicationSubmittedDate);
+};
+
+const Header = {
+  columns: [
+    { title: 'Name', field: 'fullName', width: '20%' },
+    { title: 'Email', field: 'email', width: '20%' },
+    { title: 'Mobile', field: 'mobileNumber' },
+    { title: 'Age', field: 'age' },
+    { title: 'Academy Location', field: 'location' },
+    { title: 'Address', field: 'address' },
+    {
+      title: 'Profile',
+      // eslint-disable-next-line react/prop-types
+      render: ({ _id }) => (
+        <Link
+          className="profile_Link"
+          exact
+          to={`/dashboard/applications/submitted/${_id}`}
+        >
+          View
+        </Link>
+      ),
+    },
+  ],
+};
+
+const Completed = () => {
+  const [submitted, setSubmitted] = useState();
+
+  useEffect(() => {
+    if (!submitted) getSubmitted().then(setSubmitted);
+  }, [submitted]);
+
+  return (
+    <>
+      <Helmet>
+        <title>Completed Applications</title>
+      </Helmet>
+      <img src={backGround} alt="backGround" className="dash__background" />
+      <div className="dashboard__page">
+        <div className="submitted__container">
+          <MaterialTable
+            className="submitted__table"
+            title="Submitted Applications"
+            columns={Header.columns}
+            data={submitted}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Completed;
