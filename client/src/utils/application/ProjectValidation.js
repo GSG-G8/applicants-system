50140a3a09a79ade0d4aa10087fb78196ff62f19
ha77an <ha77an19@gmail.com>
2020-06-08 14:56:13 +0300

    Fixed project link in Final project link [validation]

--- client/src/utils/application/ProjectValidation.js
+++ client/src/utils/application/ProjectValidation.js
@@ -6,7 +6,7 @@ const schema = yup.object().shape({
     .trim()
     .required('Enter your GitHub Link')
     .matches(
-      /^([A-Za-z0-9]+@|http(|s):\/\/)([A-Za-z0-9.]+(:\d+)?)(?::|\/)([\d/\w.-]+?)(\.git)?$/i,
+      /^(https):\/\/(github.com)\/[a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+$/,
       'Github Not Match'
     ),
 });
